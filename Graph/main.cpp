358ee746d0cd77c02278a2e4dbf7178dfb4a59e2
Denis Zangarov <denisimo_98@yahoo.com>
2022-05-08 17:14:40 +0300

    Update main.cpp

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -3,30 +3,24 @@
 
 void dg_test() {
 	Graph<int> graph(std::vector<std::pair<std::vector<int>, bool>> {
+        { {10, 11}, true },
         { {1, 2}, true },
-        { {9,5}, false},
-        { {9,8}, true },
-        { {9,10}, true },
-        { {14,5}, false },
-        { {14,6}, false },
-        { {7,8}, true },
-        { {14,6}, false },
-        { {2,3}, true },
-        { {3,4}, true },
-        { {4,5}, true },
-        { {10,14}, false },
-        { {5,6}, true },
-        { {3,4}, true },
-        { {1,2}, true },
-        { {2,3}, true },
-        { {1,7}, true },
+        { {2,4}, true },
+        { {7,5}, false },
+        { {5,1}, true },
+        { {3,1}, false },
+        { {11,3}, false },
+        { {5,8}, false },
+        { {3,2}, false },
+        { {3,4}, false },
+        { {8,10}, false },
     });
 
 	graph.print_graph();
     std::cout << "\n";
 	graph.print_sorted_graph();
     std::cout << "\n";
-    graph.print_shortest_path(1,14);
+    graph.print_shortest_path(1,3);
 }
 
 int main() {
